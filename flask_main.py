37a93c65dd09e18322d3bf22b74d8fedece2124a
Josef-TL <j.tonnesenlucas@gmail.com>
2024-09-29 12:25:35 +0200

    init commit, lotsa changes

---
+++ flask_main.py
@@ -0,0 +1,55 @@
+import data_dict
+import  sqlite3
+from flask import Flask, request, jsonify, g
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+# Get all
+@app.route('/members')
+def get_members():
+    con = sqlite3.connect("flask_ex.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM members")
+    members = cur.fetchall()
+    
+    con.close()
+
+    return jsonify(members)
+
+# Get one
+@app.route('/members/<int:id>')
+def get_member(member_id):
+    con = sqlite3.connect("flask_ex.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM members WHERE rowid = ?",(member_id))
+    member = cur.fetchall()
+    
+    con.close()
+    
+    return jsonify(member)
+
+@app.route('/api/members/create', methods=['POST'])
+def create_member():
+    
+    new_member = request.get_json()
+
+
+    conn = sqlite3.connect('flask_ex.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO members VALUES(?,?,?,?,?,?,?,?,?,?,?)", tuple(new_member.values()))
+    conn.commit()
+
+    return jsonify(new_member), 201
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
